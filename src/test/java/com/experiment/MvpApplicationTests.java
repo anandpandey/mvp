fb3f6b26e1d0d8c8d6d93c217869785a3f80569f
Anand Pandey <anand.etalk@gmail.com>
2015-11-11 18:06:53 +0530

    Basic Spring Boot Project setup

---
+++ src/test/java/com/experiment/MvpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.experiment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MvpApplication.class)
+public class MvpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
